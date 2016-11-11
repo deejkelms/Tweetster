80d6b4d95a29f773067a7f6d38a0febd742349c9
Derek Kelmanson <dkelmanson@gmail.com>
2016-11-11 02:40:19 +0000

    fixed explore page styling

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,12 +1,22 @@
+# The pages controller contains all of the code for any page inside of /pages
 class PagesController < ApplicationController
+  
+  # back-end code for pages/index
   def index
   end
 
+  # back-end code for pages/home
   def home
-    @posts = Post.all
+    following = Array.new
+    # for @f in current_user.following do
+    #   following.push(@f.id)
+    # end
+
+    @posts = Post.where("user_id IN (?)", following)
     @newPost = Post.new
   end
 
+  # back-end code for pages/profile
   def profile
     # grab the username from the URL as :id
     if (User.find_by_username(params[:id]))
@@ -18,9 +28,15 @@ class PagesController < ApplicationController
     
     @posts = Post.all.where("user_id = ?", User.find_by_username(params[:id]).id)
     @newPost = Post.new
+    
+    # @toFollow = User.all.last(5)
   end
 
+  # back-end code for pages/explore
   def explore
     @posts = Post.all
+    @newPost = Post.new
+    # @toFollow = User.all.last(5)
   end
+  
 end
